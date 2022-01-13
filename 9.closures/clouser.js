c4efff1eed4f7a7e64986b60f4c4fd906f7ad40e
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-01-13 10:37:13 +0530

    added global variables

--- 9.closures/clouser.js
+++ 9.closures/clouser.js
@@ -11,3 +11,14 @@ var wishMe = sayHello('Uma')
 console.log(wishMe)
 
 console.log(greet('Welcome')('Uma'))
+
+function parent(name, city) {
+    var a =10;
+    var b = 20
+
+ return  function(){
+        return a + b + name + city
+    }
+}
+
+console.log(parent('uma', 'hyd')())
